a45504c35546b0511f4eed6ce08cd4a4d161aae5
Sahil Dhanjal <53312852+Sahil-Dhanjal@users.noreply.github.com>
2022-10-18 23:04:06 +0530

    Time: 24 ms (67.19%), Space: 10.7 MB (54.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,48 @@
 class Solution {
 public:
+    
+    // APPROACH - 1
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> answer;
+        
+//         for(int i = 0; i < nums.size(); i++){
+//             for(int j = i + 1; j < nums.size(); j++){
+//                 if(nums[i]+nums[j] == target){
+//                     answer.push_back(i);
+//                     answer.push_back(j);
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return answer;
+//     }
+    
+    // APPROACH - 2
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> answer;
+        // We're going to store the Vector's element and it's corresponding Index in the map.
+        unordered_map<int,int> mp;
         
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                if(nums[i]+nums[j] == target){
-                    answer.push_back(i);
-                    answer.push_back(j);
-                    break;
-                }
-            }
+        // Inserting all the elements of the given vector into the Unordered map & side by side finding the another element to complete the pair
+        // O(n)
+        for(int i = 0; i < nums.size(); ++i){
+            // first check - is the another element present in the map to complete the target sum
+            int currentElement = nums[i];
+            int remainingTargetSumRequired = target - currentElement;
+            
+            if(mp.find(remainingTargetSumRequired) != mp.end()){
+                // return the current Element's index & the index of the element thaht completed the remaining sum.
+                return { i , mp[remainingTargetSumRequired] };
+            } 
+            
+            // If not found - no problem!
+            // Simply move ahead but before that do insert the proper mapping in the Unordered map.
+            // This could've been also done before the 'if part' as well but we didn't as this could've made us run into the same element forming the tsarget sum case!
+            mp[currentElement] = i;
         }
+
         
-        return answer;
+        // Returning an empty vector as the Sum was not found.
+        return {};
     }
 };
