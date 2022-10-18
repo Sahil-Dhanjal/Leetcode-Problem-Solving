1f1682046447e74e5c37f3ca76fde6e04c7bc372
Sahil Dhanjal <53312852+Sahil-Dhanjal@users.noreply.github.com>
2022-10-18 23:30:11 +0530

    Time: 37 ms (24.19%), Space: 5.9 MB (74.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,26 +1,67 @@
 class Solution {
 public:
+    
+    // Approach 1: Store all the digits in a vector and use the two pointer approach (start < end) to check if the digits match from either side.
+    // Uses O(n) extra space.
+//     bool isPalindrome(int x) {
+        
+//         if(x < 0){
+//             return false;
+//         }
+        
+//         vector<int> number;
+        
+//         while(x!=0){
+//             number.push_back(x%10);
+//             x = x/10;
+//         }
+        
+//         int start = 0;
+//         int end = number.size() - 1;
+//         while(start < end){
+//             if(number[start++]!=number[end--]){
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+    
+    // Approach 2: Reverse the given integer and then compare it to its reverse. If they're equal therefore the given integer was a palindrome, otherwise not.
+    // Approach 2 uses no extra space while approach 1 does.
+    int reverse(int x){
+        
+        // Declared long long int to prevent the case of integer overflow.
+        long long int rev = 0;
+        
+        while(x!=0){
+            // get the last digit
+            int last = x%10;
+            
+            // Add it to the integer rev so that the digit comes to the front
+            rev = ( (rev*10) + last);
+            
+            // Cut down given integer's size to move to its next unit integer
+            x = x /10;
+        }
+        
+        return rev;
+    }
+    
     bool isPalindrome(int x) {
         
+        
         if(x < 0){
+            // Because of the Assumption that the negative numbers are non-palindromic
             return false;
         }
         
-        vector<int> number;
+        int rev = reverse(x);
         
-        while(x!=0){
-            number.push_back(x%10);
-            x = x/10;
+        if(rev == x){
+            return true;
+        }else{
+            return false;
         }
-        
-        int start = 0;
-        int end = number.size() - 1;
-        while(start < end){
-            if(number[start++]!=number[end--]){
-                return false;
-            }
-        }
-        
-        return true;
     }
 };
